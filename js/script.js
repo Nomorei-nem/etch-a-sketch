9ccfc0a6fc994f5aab0d6d213ef9066d3b4a3675
NomoreiMe <eldomatt2017@gmail.com>
2024-02-10 19:00:42 +0530

    Add random disappearing colors to grid

--- js/script.js
+++ js/script.js
@@ -2,6 +2,24 @@
 
 const gridContainer = document.querySelector('.container');
 
+const randomColor = function () {
+	const rgbValue = () => Math.floor(Math.random() * 256);
+
+	return `rgb(${rgbValue()} ${rgbValue()} ${rgbValue()})`;
+};
+
 for (let i = 1; i <= 16; i++) {
-	gridContainer.innerHTML += `<div class="flex-item" data-${i}>${i}</div>`;
+	gridContainer.innerHTML += `<div class="grid-pixel pixel-no${i}" data-${i}></div>`;
 }
+
+const gridPixel = Array.from(document.querySelectorAll('.grid-pixel'));
+
+for (let i = 0; i < gridPixel.length; i++) {
+	gridPixel[i].addEventListener('mouseover', (e) => {
+		e.target.style = `background-color: ${randomColor()}`;
+
+		setTimeout(() => (e.target.style = 'background-color: none'), 700);
+	});
+}
+
+console.log(randomColor());
