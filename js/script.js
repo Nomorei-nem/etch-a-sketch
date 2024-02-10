b32bea08adc30508fc2a71777fda941a343cf218
NomoreiMe <eldomatt2017@gmail.com>
2024-02-10 16:16:26 +0530

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const gridContainer = document.querySelector('.container');
+
+for (let i = 1; i <= 16; i++) {
+	gridContainer.innerHTML += `<div class="flex-item" data-${i}>${i}</div>`;
+}
