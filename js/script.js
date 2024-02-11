2370ec565b7a711f105544393009b7bd55c1479b
NomoreiMe <eldomatt2017@gmail.com>
2024-02-11 07:17:48 +0530

    Add customizable grid width

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const gridContainer = document.querySelector('.container');
+let gridContainer = document.querySelector('.container');
+const gridResolution = document.querySelector('.grid-resolution');
 
 const randomColor = function () {
 	const rgbValue = () => Math.floor(Math.random() * 256);
@@ -9,17 +10,48 @@ const randomColor = function () {
 };
 
 for (let i = 1; i <= 16; i++) {
-	gridContainer.innerHTML += `<div class="grid-pixel pixel-no${i}" data-${i}></div>`;
+	gridContainer.innerHTML += `<div style="flex: 1 0 25%" class="grid-pixel pixel-no${i}" data-${i}></div>`;
 }
 
-const gridPixel = Array.from(document.querySelectorAll('.grid-pixel'));
+let gridPixel = Array.from(document.querySelectorAll('.grid-pixel'));
 
 for (let i = 0; i < gridPixel.length; i++) {
 	gridPixel[i].addEventListener('mouseover', (e) => {
-		e.target.style = `background-color: ${randomColor()}`;
+		e.target.style = `background-color: ${randomColor()}; flex: 1 0 25%`;
 
-		setTimeout(() => (e.target.style = 'background-color: none'), 700);
+		setTimeout(
+			() => (e.target.style = 'background-color: none; flex: 1 0 25%'),
+			700
+		);
 	});
 }
 
-console.log(randomColor());
+gridResolution.addEventListener('submit', (e) => {
+	const resolution = document.querySelector('.resolution');
+	const userResolution = +resolution.value;
+	const newPixelWidth = 100 / userResolution;
+
+	resolution.blur();
+	e.preventDefault();
+
+	gridContainer.innerHTML = '';
+
+	for (let i = 1; i <= userResolution * userResolution; i++) {
+		gridContainer.innerHTML += `<div style="flex: 1 0 ${newPixelWidth}%" class="grid-pixel pixel-no${i}" data-${i}></div>`;
+	}
+
+	gridPixel = Array.from(document.querySelectorAll('.grid-pixel'));
+
+	for (let i = 0; i < gridPixel.length; i++) {
+		gridPixel[i].addEventListener('mouseover', (e) => {
+			e.target.style = `background-color: ${randomColor()}; flex: 1 0 ${newPixelWidth}%`;
+
+			console.log(e.target.style);
+			setTimeout(
+				() =>
+					(e.target.style = `background-color: none; flex: 1 0 ${newPixelWidth}%`),
+				700
+			);
+		});
+	}
+});
