a89295fc4fb843c3593a562154157928debeaa59
NomoreiMe <eldomatt2017@gmail.com>
2024-02-25 13:40:49 +0530

    Redo JavaScript with event delegation

--- js/script.js
+++ js/script.js
@@ -241,7 +241,7 @@ rainbowBtn.addEventListener('click', (e) => {
 	rainbowMode();
 });
 
-// Eventlistner for Color Mode Button
+// Eventlistener for Color Mode Button
 colorBtn.addEventListener('click', (e) => {
 	e.preventDefault();
 
